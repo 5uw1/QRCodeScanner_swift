559bf384927f3cd566d68fbd086e41c7273b5eb2
5uw1 <suwijakza@gmail.com>
2023-08-06 18:58:53 +0200

    Initial Commit

---
+++ QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by Suwijak Thanawiboon on 06.08.23.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
